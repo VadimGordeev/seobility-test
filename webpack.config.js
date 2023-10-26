758039e9a8f9ec633c9220a9b116ce101f0a0cb9
VadimGordeev <vadimgordeev22@gmail.com>
2023-10-26 15:40:56 +0300

    build: fix scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
-const webpack = require("webpack");
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: "./src/script/index.js",
+  entry: './src/script/index.js',
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
   },
-  mode: "development",
+  mode: 'development',
   devServer: {
     port: 9000,
     hot: true,
     static: {
-      directory: path.join(__dirname, "./dist"),
-    },
+      directory: path.join(__dirname, './dist')
+    }
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader'
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-    ],
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      }
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "styles.css",
+      filename: 'styles.css'
     }),
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: 'src/index.html'
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin()
   ],
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "src"),
-    },
-  },
+      '~': path.resolve(__dirname, 'src')
+    }
+  }
 };
